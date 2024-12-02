880fb725cbfb9c5dac9685d799862c289303bdff
Guardianprime <okongordian@gmail.com>
2024-12-02 15:51:12 +0100

    styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,66 @@
+import styles from './Footer.module.css';
+
 function Footer() {
     return (
-        <footer>
-            <div>
+        <footer className={styles.footer}>
+            <div className={styles.children}>
                 <h4>Exclusive</h4>
-                <ul>
-                    <li>Subscribe</li>
-                    <li>
+                <ul className={styles.grandChildren}>
+                    <li className={styles.listItems}>Subscribe</li>
+                    <li className={styles.listItems}>
                         <p>Get 10% off your first order</p>
                         <input type="text" />
                     </li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.children}>
                 <h4>Support</h4>
-                <ul>
-                    <li>120 ogunsanya drive surulere, lagos NIgeria</li>
-                    <li>bandriid@gmail.com</li>
-                    <li>+234 8177776060</li>
+                <ul className={styles.grandChildren}>
+                    <li className={styles.listItems}>120 ogunsanya drive surulere, lagos NIgeria</li>
+                    <li className={styles.listItems}>bandriid@gmail.com</li>
+                    <li className={styles.listItems}>+234 8177776060</li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.children}>
                 <h4>Account</h4>
-                <ul>
-                    <li>My Account</li>
-                    <li>Login / Register</li>
-                    <li>Cart</li>
-                    <li>Wishlist</li>
-                    <li>Shop</li>
+                <ul className={styles.grandChildren}>
+                    <li className={styles.listItems}>My Account</li>
+                    <li className={styles.listItems}>Login / Register</li>
+                    <li className={styles.listItems}>Cart</li>
+                    <li className={styles.listItems}>Wishlist</li>
+                    <li className={styles.listItems}>Shop</li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.children}>
                 <h4>Quick Link</h4>
-                <ul>
-                    <li>Privacy Policy</li>
-                    <li>Terms Of Use</li>
-                    <li>FAQ</li>
-                    <li>Contact</li>
+                <ul className={styles.grandChildren}>
+                    <li className={styles.listItems}>Privacy Policy</li>
+                    <li className={styles.listItems}>Terms Of Use</li>
+                    <li className={styles.listItems}>FAQ</li>
+                    <li className={styles.listItems}>Contact</li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.children}>
                 <h4>Download App</h4>
-                <div>
-                    <div>qr code</div>
-                    <div>
-                        <div>Get it on G</div>
-                        <div>Get it on APP store</div>
+                <p className={styles.para}>Save $3 with app new users only</p>
+                <div className={styles.allImageContainer}>
+                    <div className={styles.imgContainer}>
+                        <img src="qrcode.svg" className={styles.svg} />
+                    </div>
+                    <div className={styles.appImageContainer}>
+                        <div>
+                            <img className={styles.appstoreImage} src="play.jpg" />
+                        </div>
+                        <div >
+                            <img className={styles.playstoreImage} src="applestore.jpg" />
+                        </div>
                     </div>
                 </div>
-                <ul>
-                    <li>F</li>
-                    <li>X</li>
-                    <li>IG</li>
-                    <li>LI</li>
+                <ul className={styles.iconContainer}>
+                    <li><i className="fa-brands fa-facebook"></i></li>
+                    <li><i className="fa-brands fa-x-twitter"></i></li>
+                    <li><i className="fa-brands fa-instagram"></i></li>
+                    <li><i className="fa-brands fa-linkedin"></i></li>
                 </ul>
             </div>
         </footer>
